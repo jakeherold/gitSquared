e713b30cd3e698dbe63162349875959a1d130cb2
Jacob Porter Herold <dcejake@gmail.com>
2015-11-17 11:12:12 -0800

    fixed logic gaps

--- squared.js
+++ squared.js
@@ -30,7 +30,9 @@ var openTerminal = function (){
     urlCopied = true;
     document.getElementById("moduleOneOutput").innerHTML = "You copy the URL";
     moduleOnePicture.src = "assets/images/copyURL.png";
-  } else {
+  } else if ((gitHubOpen=== false) && (urlCopied === false)){
+    document.getElementById("moduleOneOutput").innerHTML = "Open GitHub first";
+  } else if (urlCopied=== true){
     document.getElementById("moduleOneOutput").innerHTML = "You've already copied the URL!";
   }
  }
@@ -53,14 +55,16 @@ var enterGitCloneCommand = function (){
      document.getElementById("moduleOneOutput").innerHTML = "Awesome! You got everything just right! You're now the pround owner of a local copy of your repository!";
      moduleOnePicture.src = "assets/images/typeGitClone.png";
      moduleOneComplete = true;
-  } else if ((terminalOpen === false) && (urlCopied === true) && (terminalRightPlace === true)){
-     document.getElementById("moduleOneOutput").innerHTML = "Your terminal isn't open. Try doing that first.";
+  } else if ((terminalOpen === true) && (urlCopied === false) && (terminalRightPlace === false)){
+     document.getElementById("moduleOneOutput").innerHTML = "Your terminal is open. Now get your URL and pick where to put the file";
   } else if ((terminalOpen === true) && (urlCopied === true) && (terminalRightPlace === false)){
      document.getElementById("moduleOneOutput").innerHTML = "Your terminal is open, and you have your URL. But, you don't have your terminal navigated to where you want it. Try doing that first.";
   } else if ((terminalOpen === true) && (urlCopied === false) && (terminalRightPlace === true)){
      document.getElementById("moduleOneOutput").innerHTML = "You haven't gotten the URL for your repository yet. Go do that, and try again!";
   } else if ((terminalOpen === false) && (urlCopied === false) && (terminalRightPlace === false)){
      document.getElementById("moduleOneOutput").innerHTML = "You haven't done anything yet. Try opening the terminal, and getting the URL for your repository";
+  }else if ((terminalOpen === false) && (urlCopied === true) && (terminalRightPlace === false)){
+     document.getElementById("moduleOneOutput").innerHTML = "You haven't opened the terminal, yet. Do that first. ";
   }
   else {
     document.getElementById("moduleOneOutput").innerHTML = "Not ever the devs know whats going on or how you got here. Here there be dragons. Godspeed, foolish mortal.";
