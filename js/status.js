5cb6f9be8241f7bbbf2dfd36ab7a40c9a0e4375f
Jacob Porter Herold <dcejake@gmail.com>
2015-11-17 23:40:53 -0800

    added Status Module, and wrote blurb for Checkout

---
+++ js/status.js
@@ -0,0 +1,8 @@
+var solidJob = function(){
+  document.getElementById("LessonTwoModuleOneOutput").innerHTML = "Excellent work. Now head on down to the next module!";
+  LessonTwoModuleOnePicture.src = "assets/images/niceWork.gif";
+}
+var reread = function(){
+  document.getElementById("LessonTwoModuleOneOutput").innerHTML = "Uh oh! Go re-read the tutorial, and try out 'git status' in your terminal"
+  LessonTwoModuleOnePicture.src = "assets/images/panic.gif"
+}
