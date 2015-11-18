3d6957f2cc19e5e0475538e5e240e4cb30a715ea
Jessica Tran Hur <jesshur824@gmail.com>
2015-11-18 12:55:59 -0800

    first commit of branch.js, which includes the logics for the test inside git branch module

---
+++ js/branch.js
@@ -0,0 +1,114 @@
+$(".intro:not(#intro1)").css({"display": "none"});
+$(".messageBranch").css({"display": "none"});
+counter = 1;
+function branchTest(){
+  var $message = $(".messageBranch");
+  var $correct = $("#correctBranch");
+  var $sorry = $("#sorryBranch");
+  var intro = "";
+  var $numIntro = $(".intro");
+  var $userInput = $("#gitBranchInput").val();
+  switch (counter){
+    case 2:
+      if ($userInput=="git checkout -b mistake_branch" ){
+        counter++;
+        console.log("counter is now: "+counter);
+        $message.css({"display": "none"});
+        $correct.css({"display": "block"});
+        intro = intro.concat("#intro",counter);
+        console.log("intro value after concat is: "+intro);
+        $numIntro.css({"display": "none"});
+        $(intro).css({"display": "block"});
+        intro = "";
+      }
+      else{
+        $message.css({"display": "none"});
+        $sorry.css({"display": "block"});
+      }
+      break;
+    case 3:
+      if ($userInput=="git branch" ){
+        counter++;
+        console.log("counter is now: "+counter);
+        $message.css({"display": "none"});
+        $correct.css({"display": "block"});
+        console.log("counter input is: "+counter);
+        intro = intro.concat("#intro",counter);
+        $numIntro.css({"display": "none"});
+        $(intro).css({"display": "block"});
+        intro = "";
+      }
+      else{
+        $message.css({"display": "none"});
+        $sorry.css({"display": "block"});
+      }
+      break;
+    case 4:
+      if ($userInput=="git checkout master"){
+        counter++;
+        console.log("counter is now: "+counter);
+        $message.css({"display": "none"});
+        $correct.css({"display": "block"});
+        console.log("counter input is: "+counter);
+        intro = intro.concat("#intro",counter);
+        $numIntro.css({"display": "none"});
+        $(intro).css({"display": "block"});
+        intro = "";
+      }
+      else{
+        $message.css({"display": "none"});
+        $sorry.css({"display": "block"});
+      }
+      break;
+    case 5:
+      if ($userInput=="git branch -d mistake_branch" || $userInput=="git branch -D mistake_branch" ){
+        counter++;
+        console.log("counter is now: "+counter);
+        $message.css({"display": "none"});
+        $correct.css({"display": "block"});
+        intro = intro.concat("#intro",counter);
+        $numIntro.css({"display": "none"});
+        $(intro).css({"display": "block"});
+        intro = "";
+      }
+      else{
+        $message.css({"display": "none"});
+        $sorry.css({"display": "block"});
+      }
+      break;
+    case 6:
+      counter++;
+      intro = intro.concat("#intro",counter);
+      $numIntro.css({"display": "none"});
+      $(intro).css({"display": "block"});
+      intro = "";
+      break;
+    default:
+      break;
+  }
+  if (counter===1){
+    if ($userInput=="git branch css" ){
+      $(".intro").css({"display": "none"});
+      $("#intro2").css({"display": "block"});
+      counter++;
+      console.log("counter is now: "+counter);
+      $message.css({"display": "none"});
+      $correct.css({"display": "block"});
+    }
+    else{
+      $message.css({"display": "none"});
+      $sorry.css({"display": "block"});
+    }
+  }
+  console.log("reached end of function.");
+  if (counter > $numIntro.length){
+    console.log("reached end of function. Restarting counter");
+    counter = 1;
+    console.log("counter is now: "+counter);
+    $message.css({"display": "none"});
+    $(".intro").css({"display": "none"});
+    $("#intro1").css({"display": "block"});
+  }
+}
+
+$("#enterCommand").on("click",branchTest);
